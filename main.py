dbb13124a87dc03025ecee2d41ce39d87bb8b3a7
adi elron <adielrontw@gmail.com>
2021-10-19 16:02:38 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template,abort, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
-
+import os
+from dotenv import load_dotenv
 from sqlalchemy import ForeignKey, Integer, Column
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -13,8 +14,10 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 
-
-
+load_dotenv('.env')
+SECRET_KEY = os.getenv("NAME")
+print(os.environ['PASSWORD'])
+print(SECRET_KEY)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -26,7 +29,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-login_manager=LoginManager()
+login_manager = LoginManager()
 login_manager.init_app(app)
 
 
