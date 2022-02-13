caba69f06700ce2d3075ee0c21852e48c86d9487
adielron <91066343+adielron@users.noreply.github.com>
2022-02-13 16:51:23 +0200

    finished v1+db

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from functools import wraps
 import random
 import stripe
 
-stripe.api_key = 'sk_test_51KSipGINEFASbnNK0aMM2j36D43GLJmDfYYIaRw0zgLyHMtjdpVO8cr2NzPES2kS3S2uLnG8ResHiMz2eBhBgwoT006ZNnC7VL'
+stripe.api_key = os.environ['stripe']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -191,82 +191,6 @@ def logout():
     logout_user()
     print('user logged out')
     return redirect(url_for('home'))
-
-
-
-
-# @app.route('/register',methods=["POST","GET"])
-# def register():
-#     form=Register_user()
-#     if form.validate_on_submit():
-#         if User.query.filter_by(email=form.email.data).first():
-#             flash('email alredy exists')
-#             return redirect(url_for('login'))
-#         else:
-#             password_hash = generate_password_hash(
-#                 form.password.data,
-#                 method='pbkdf2:sha256',
-#                 salt_length=8)
-#
-#             user=User(
-#                 email=form.email.data,
-#                 password=password_hash,
-#                 name=form.name.data,
-#             )
-#
-#             db.session.add(user)
-#             db.session.commit()
-#             login_user(user)
-#         return redirect(url_for('get_all_posts'))
-#
-#
-#
-#     return render_template("register.html",form=form)
-#
-#
-# @app.route('/login',methods=["POST","GET"])
-# def login():
-#     form=Login()
-#     if form.validate_on_submit():
-#         email=form.email.data
-#         user=User.query.filter_by(email=email).first()
-#         if user:
-#             if check_password_hash(user.password, form.password.data):
-#                 login_user(user=user)
-#                 return redirect(url_for('get_all_posts',loged_in=True))
-#             else:
-#                 flash('passwords dont match.', 'error')
-#                 redirect('login')
-#         else:
-#             flash('email does not exist.','error')
-#             redirect('login')
-#
-#
-#     return render_template("login.html",form=form)
-
-
-
-
-
-
-
-# @app.route("/create-item",methods=["GET","POST"])
-# @admin
-# def add_new_post():
-#     form = Product()
-#     if form.validate_on_submit():
-#         new_post = Product(
-#
-#         )
-#         db.session.add(new_post)
-#         db.session.commit()
-#         return redirect(url_for("get_all_posts"))
-#     return render_template("make-post.html", form=form, current_user=current_user)
-#
-#
-
-
-
 
 
 if __name__ == "__main__":
